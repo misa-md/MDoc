abedbff724dd905ec74bd0cdcc459739e8ae6c77
genshen <genshenchu@gmail.com>
2019-07-24 22:53:29 +0800

    add docusaurus documentation package.

---
+++ website/core/Footer.js
@@ -0,0 +1,95 @@
+/**
+ * Copyright (c) 2017-present, Facebook, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+const React = require('react');
+
+class Footer extends React.Component {
+  docUrl(doc, language) {
+    const baseUrl = this.props.config.baseUrl;
+    const docsUrl = this.props.config.docsUrl;
+    const docsPart = `${docsUrl ? `${docsUrl}/` : ''}`;
+    const langPart = `${language ? `${language}/` : ''}`;
+    return `${baseUrl}${docsPart}${langPart}${doc}`;
+  }
+
+  pageUrl(doc, language) {
+    const baseUrl = this.props.config.baseUrl;
+    return baseUrl + (language ? `${language}/` : '') + doc;
+  }
+
+  render() {
+    return (
+      <footer className="nav-footer" id="footer">
+        <section className="sitemap">
+          <a href={this.props.config.baseUrl} className="nav-home">
+            {this.props.config.footerIcon && (
+              <img
+                src={this.props.config.baseUrl + this.props.config.footerIcon}
+                alt={this.props.config.title}
+                width="66"
+                height="58"
+              />
+            )}
+          </a>
+          <div>
+            <h5>Docs</h5>
+            <a href={this.docUrl('getting-started.html', this.props.language)}>
+              Getting Started
+            </a>
+            <a href={this.docUrl('guide.html', this.props.language)}>
+              Guides
+            </a>
+            <a href={this.docUrl('api.html', this.props.language)}>
+              API Reference
+            </a>
+          </div>
+          <div>
+            <h5>Community</h5>
+            <a href={this.pageUrl('users.html', this.props.language)}>
+              User Showcase
+            </a>
+            <a
+              href="https://git.hpcer.dev/HPCer/CrystalMD/CrystalMD/issues"
+              target="_blank"
+              rel="noreferrer noopener">
+              Issues
+            </a>
+            <a href="https://hub.hpcer.dev/">Project Chat</a>
+          </div>
+          <div>
+            <h5>More</h5>
+            <a href={`${this.props.config.baseUrl}blog`}>Blog</a>
+            <a href={this.props.config.repoUrl}>GitHub</a>
+            <a
+              className="github-button"
+              href={this.props.config.repoUrl}
+              data-icon="octicon-star"
+              data-show-count="false">
+              Star
+            </a>
+          </div>
+        </section>
+
+        <a
+          href="https://git.hpcer.dev/HPCer/"
+          target="_blank"
+          rel="noreferrer noopener"
+          className="fbOpenSource">
+          <img
+            src={`${this.props.config.baseUrl}img/oss_logo.svg`}
+            alt="HPCDE lab"
+            width="120"
+            height="72"
+          />
+        </a>
+        <section className="copyright">{this.props.config.copyright}</section>
+      </footer>
+    );
+  }
+}
+
+module.exports = Footer;
